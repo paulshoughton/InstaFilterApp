f3d7ab232da9c06425fe103458c173d2dd10bfc8
Paul Houghton <paul.houghton@gmail.com>
2020-11-05 08:14:34 +0000

    Initial Commit

---
+++ InstaFilterApp/InstaFilterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterAppApp.swift
+//  InstaFilterApp
+//
+//  Created by Paul Houghton on 05/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
